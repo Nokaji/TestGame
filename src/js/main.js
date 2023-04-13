71f1aadd0dbadb7c511d57e833f75f7295a1dd77
Nokaji <nokaji.dev@gmail.com>
2023-04-13 18:35:14 +0200

    update

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+const config = {
+    width: 500,
+    height: 300,
+    type: Phaser.AUTO
+}
+
+var game = new Phaser.Game(config)
+
+function preload(){
+    this.image.load
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
